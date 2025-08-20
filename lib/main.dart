6c7a557374b5d55ccbbf2e0f508609cc4537b434
jettinsha <jettinsha@gmail.com>
2025-08-20 17:05:24 +0530

    nothing

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Hotbox Kitchen',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: Orders(),
+      home: HotboxKitchenScreen(),
     );
   }
 }
